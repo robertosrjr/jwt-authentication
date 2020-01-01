764ab6425ac5d0ce3360edb6e474a798911cf6ed
Roberto S. Ramos Jr <robertosrjr@gmail.com>
2020-01-01 15:49:34 -0300

    versão inicial do springBoot

---
+++ src/test/java/com/gmail/robertosrjr/jwt/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.robertosrjr.jwt.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
